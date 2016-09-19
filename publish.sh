7cd9a79b429f2481aa10567cafc010bcaea6b70d
ChrisRackauckas <me@chrisrackauckas.com>
2016-09-18 17:11:13 -0700

    Update Slides

--- publish.sh
+++ publish.sh
@@ -1,4 +1,4 @@
-!/bin/bash
+#!/bin/bash
 
 # Generate the Slides and Pages
 jupyter-nbconvert Notebooks/Index.ipynb --reveal-prefix=reveal.js
@@ -15,29 +15,32 @@ for f in "${arr[@]}"; do
 
    # Convert the Notebook to HTML
    jupyter-nbconvert --to html Notebooks/"$filename".ipynb
-   # Move to the html directory
-   mv Notebooks/"$filename".html  Rendered/"$filename".html
+   # Move to the Html directory
+   mv Notebooks/"$filename".html  Html/"$filename".html
 
    # Convert the Notebook to slides
    jupyter-nbconvert --to slides Notebooks/"$filename".ipynb --reveal-prefix=reveal.js
-   # Move to the slides directory
+   # Move to the Slides directory
    mv Notebooks/"$filename".slides.html  Slides/"$filename".html
 
    # Convert the Notebook to Markdown
    jupyter-nbconvert --to markdown Notebooks/"$filename".ipynb
-   # Move to the html directory
+   # Move to the Markdown directory
    mv Notebooks/"$filename".md  Markdown/"$filename".md
 
    # Convert the Notebook to Latex
    jupyter-nbconvert --to latex Notebooks/"$filename".ipynb
+   # Move to the Tex directory
    mv Notebooks/"$filename".tex  Tex/"$filename".tex
 
    # Convert the Notebook to Pdf
-   cd Notebooks
+   cp Notebooks/"$filename".ipynb src/"$filename".ipynb
+   cd src
    jupyter-nbconvert --to pdf "$filename".ipynb
    # Move to the html directory
    mv "$filename".pdf  ../Pdfs/"$filename".pdf
-   cd .. 
+   rm "$filename".ipynb
+   cd ..
 done
 
 # Push the updates to gh-pages
